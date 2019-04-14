6fb9538aee9f74688514d61325af730e7c817049
LD <LDinho@users.noreply.github.com>
2019-04-14 02:44:41 -0400

    add TodoForm for adding new todos via useRef hook
    user is now able to add new tasks/todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import './App.css';
-import { toggle_todo } from './actions';
-import TodoList from './components/TodoList'
+import { toggle_todo, add_todo } from './actions';
+import TodoList from './components/TodoList';
+import TodoForm from './components/ToDoForm';
 
 class App extends Component {
 
   handleClickToggle = (id) => {
     this.props.toggle_todo(id);
+  }
+
+  handleSubmit = (newTask) => {
+    this.props.add_todo(newTask);
   }
 
   render() {
@@ -21,6 +26,8 @@ class App extends Component {
               - TodoOrNotTodo -
             </h1>
         </header>
+
+        <TodoForm onFormSubmit={this.handleSubmit} />
 
         <TodoList tasks={tasks}
                   onTaskClickHandler={this.handleClickToggle}
@@ -39,6 +46,6 @@ const mapStateToProps = (state) => {
 export default connect(
   mapStateToProps,
   { toggle_todo,
-
+    add_todo,
   }
 )(App);
--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,10 +1,10 @@
 export const ADD_TODO = 'add_todo';
 export const TOGGLE_TODO = 'toggle_todo';
 
-export const add_todo = (text) => {
+export const add_todo = (newTask) => {
   return {
     type: ADD_TODO,
-    payload: text,
+    payload: newTask,
   }
 };
 
